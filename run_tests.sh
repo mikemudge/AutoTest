1bcfa9f3a9a99e6002260dccd65f96ea8b907ad2
Mike Mudge <michael@8i.com>
2016-05-31 11:12:51 +1200

    Update to handle failure assertion errors better.

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+cd /Users/mudge/projects/AutoTest
+# This has to happen in a bash script, python subprocess doesn't support source.
 source /Users/mudge/projects/virtualenv/WebPlatform/bin/activate > /dev/null 2>&1
 
-python record_tests.py
+# Optionally could pull here so we always run the most up to date version of the test runner.
+
+python record_tests.py >> automatic_tests.log 2>&1
+STATUS_CODE=$?
+
+if [ $STATUS_CODE -ne 0 ]
+then
+  echo "It all went horribly wrong" $STATUS_CODE >&2
+  cat automatic_tests.log >&2
+fi
